f18ceb70661da7e85d39f566d57148bc27096df7
Ismael Costa <ismaelcosta949@gmail.com>
2022-10-05 21:19:17 -0300

    envio2

--- index.js
+++ index.js
@@ -1,41 +1,98 @@
-var marca, produto, codigo, preco, item = ''
+let marca;
+let produto;
+let codigo;
+let preco
+
+var objeto = {marca:marca, produto:produto, codigo:codigo, preco:preco}
 var excluir = document.getElementById("codigo_remover").value
 
-// criando a função para mostrar o produto
-function Mostrar(){
-    marca = document.getElementById("marca").value
-    produto = document.getElementById("name").value
-    codigo = document.getElementById("number").value
-    preco = document.getElementById("preco").value
-
-    console.log(marca, produto, codigo, preco)
-}
+const seara = document.getElementById("lista-seara")
+const swift = document.querySelector("#lista-swift")
+const friboi = document.querySelector("#lista-friboi")
 
 // criando a função para adicionar o produto
 function addProduto(){
     confirm("Quer criar um novo produto?")
-    Mostrar()
-    if(marca == "Swift"){
-        item = document.querySelector("#lista-swift").innerHTML += "<li>Produto " + produto + ", código " + codigo + ", preco" + preco + "</li>"
-} else if(marca == "Seara"){
-    item = document.querySelector("#lista-seara").innerHTML += "<li>Produto " + produto + ", código " + codigo + ", preco" + preco + "</li>"
-} else if(marca == "Friboi"){
-    item = document.querySelector("#lista-friboi").innerHTML += "<li>Produto " + produto + ", código " + codigo + ", preco" + preco + "</li>"
-}
-    
-}
+    if(confirm("Quer criar um novo produto?") == true){
+        const opcaoMarca = document.getElementById("marca").value
+        const nomeProd = document.getElementById("name").value
+        const codProd = document.getElementById("number").value
+        const precoProd = document.getElementById("preco").value
 
-// criando a função para deletar o produto
-function removeProduto(){
-    if(confirm("Você quer mesmo excluir?") == true){
-            if(marca == "Swift"){
-                item = document.querySelector("#lista-swift").innerHTML -= "<li>Produto " +     produto + ", código " + codigo + ", preco" + preco + "</li>"
-             } else if(marca == "Seara"){
-               item = document.querySelector("#lista-seara").innerHTML -= "<li>Produto " + produto + ", código " + codigo + ", preco" + preco + "</li>"
-             } else if(marca == "Friboi"){
-               item = document.querySelector("#lista-friboi").innerHTML -= "<li>Produto " + produto + ", código " + codigo + ", preco" + preco + "</li>"
+        // passando os valores das opçoes para os objetos
+        objeto.marca = opcaoMarca
+        objeto.produto = nomeProd
+        objeto.preco = precoProd
+
+        // fazendo o if para não permitir o mesmo codigo
+        if(document.getElementById(`${codProd}`) != null){
+            alert("Cod ja existe")
+        } else{
+            objeto.codigo = codProd
+            let confirma = confirm("Adicionar?")
+            if(confirma == true){
+                seara.innerHTML +=`<li id=${objeto.codigo}> marca ${objeto.marca} nome ${objeto.nome} codigo ${objeto.codigo} preco ${objeto.preco}</li>`
             }
-    } else {
-        alert()
+          }elseif(objeto.marca=="Swift")
+        {
+            if(confirmacao==true){
+              swift.innerHTML+=`<li id=${objeto.codigo}marca ${objeto.marca}nome ${objeto.nome}cod${objeto.codigo}preco ${objeto.preco}</li>`
+            }
+         }
+          }if(objeto.marca=="Friboi"){
+      
+            if(confirmacao==true){
+      
+                friboi.innerHTML+=`<li id=${objeto.codigo}marca ${objeto.marca}nome ${objeto.nome}cod${objeto.codigo}preco ${objeto.preco}</li>`
+            }
+          }
+        }
+
+        // fazendo a função para remover os itens
+      function remove()
+      {
+      
+        const id = document.getElementById("codigo_remover").value;
+      
+        const idli = document.getElementById(`${id}`);
+        let confirma = confirm("remover?");
+      
+        if(objeto.marca=="Seara"){
+      
+          if(confirma==true){
+      
+              if(idli==null){
+                  alert('Não existe!')
+              }
+              else{
+                  liId.remove();
+              }
+          }
+        }elseif(objeto.marca=="Friboi")
+        {
+          if(confirma==true){
+              if(idli==null){
+                  alert('Não existe!')
+              }
+              else{
+                  idli.remove();
+              }
+          }
+        }elseif(objeto.marca=="Swift")
+        {
+          if(confirma==true){
+              if(idli==null){
+                  alert('Não existe!')
+              }
+              else{
+                  idli.remove();
+              }
+          }
+        }
+
+        marca.classlist.remove()
+        preco.classlist.remove()
+        produto.classlist.remove()
+        codProd.classlist.remove()
+        codigo.classlist.remove()
     }
-}
